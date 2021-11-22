c8fafa0e96fb630ec00c91330d5d831f5e8bcf9d
Shih-Hao Chen <samuelshchen@gmail.com>
2021-11-23 07:35:41 +1300

    using instead of typedef

--- body/defs.cpp
+++ body/defs.cpp
@@ -1,29 +1,28 @@
-// clang-format off
+// Hello
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-typedef long long ll;
-typedef double db;
-typedef vector<string> VS;
-typedef vector<ll> VLL;
-typedef vector<VLL> VVLL;
-typedef vector<VVLL> VVVLL;
-typedef pair<ll, ll> PLL;
-typedef map<ll, ll> MLL;
-typedef set<ll> SLL;
-typedef queue<ll> QLL;
+using ll = long long;
+using db = double;
+using VS = vector<string>;
+using VLL = vector<ll>;
+using VVLL = vector<VLL>;
+using VVVLL = vector<VVLL>;
+using PLL = pair<ll, ll>;
+using MLL = map<ll, ll>;
+using SLL = set<ll, ll>;
+using QLL = queue<ll, ll>;
+using SS = stringstream;
 
 #define rep(x, l, u) for (ll x = l; x < u; x++)
 #define rrep(x, l, u) for (ll x = l; x >= u; x--)
 #define fe(x, a) for (auto x : a)
-#define fce(x, a) for (const auto& x : a)
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define mst(x, v) memset(x, v, sizeof(x))
 #define sz(x) (ll) x.size()
-#define lg(x) (ll) x.length()
 
 #define fi first
 #define se second
@@ -33,14 +32,13 @@ typedef queue<ll> QLL;
 #define uset unordered_set
 #define mset multiset
 
-#ifdef TEST
-
+// clang-format off
 namespace Debug {
 const string open = "[", sep = ", ", close = "]";
 struct Second {}; struct First : Second {};
 
 template <typename T>
-auto one(T t, First) -> decltype(cout << t, string()) { stringstream ss; ss << t; return ss.str(); }
+auto one(T t, First) -> decltype(cout << t, string()) { SS ss; ss << t; return ss.str(); }
 template <typename T>
 auto one(T t, Second) -> decltype(t.first, t.second, string()) { return open + one(t.first, First()) + ", " + one(t.second, First()) + close; }
 template <typename T>
@@ -51,6 +49,7 @@ template <typename A, typename... B>
 string mult(A a, B... b) { return one(a, First()) + " " + mult(b...); }
 }  // namespace Debug
 
+#ifdef TEST
 #define debug(...) do { cout << Debug::mult("[" + string(#__VA_ARGS__) + "]:", __VA_ARGS__) << endl; } while (false)
 #else
 #define debug(...) do {} while (false)
@@ -61,24 +60,22 @@ ll tp(ll x) { return ( 1LL << x ); }
 ll rup(ll a, ll b) { return a % b ? a/b + 1 : a/b; }
 ll sign(ll x) {	return x == 0 ? 0 : x / abs(x); }
 ll isvowel(char c) { return string("aeiou").find(c) < 5; }
+VS ssplit(string s) { VS ans; SS ss(s); while (ss >> s) { ans.pb(s); } return ans; }
 
-void readstr(string & s, const ll len=400005) { char buf[len]; ll a = scanf("%s", buf); a++; s = buf; }
-vector<string> ssplit(string s) { vector<string> ans; stringstream ss(s); while (ss >> s) ans.pb(s); return ans; }
-template<typename A> A reversed(const A & a) { A b = a; reverse(all(b)); return b; }
-
-void makemod(ll & x, ll m) { x %= m; if (x < 0) { x += m; } }
+void makemod(ll& x, ll m) { x %= m; if (x < 0) { x += m; } }
 ll getmod(ll x, ll m) { makemod(x, m); return x; }
 ll powmod(ll a, ll b, ll m) { if (b == 0) return 1;	ll h = powmod(a, b/2, m); ll ans = h*h%m; return b%2 ? ans*a%m : ans; }
 
-template<typename A, typename B> void upmin(A & x, B v) { x = min(x, (A)v); }
-template<typename A, typename B> void upmax(A & x, B v) { x = max(x, (A)v); }
+template<typename A, typename B> void upmin(A& x, B v) { x = min(x, (A)v); }
+template<typename A, typename B> void upmax(A& x, B v) { x = max(x, (A)v); }
+template<typename A> A reversed(const A& a) { A b = a; reverse(all(b)); return b; }
 template <typename A, typename B> bool exist(const A& a, const B& b) { return a.find(b) != a.end(); }
 
 const VLL di = {0, 0, 1, -1, 1, -1, 1, -1};
 const VLL dj = {1, -1, 0, 0, -1, -1, 1, 1};
 const VLL mods = {(ll)1e9 + 7, 998244353, (ll)1e6 + 3, (ll)1e18 + 5, 1000000207};
-const ll mod = mods[0];
-const ll inf = mods[3];
+const ll mod = (ll)1e9 + 7;
+const ll inf = (ll)1e18 + 5;
 const db eps = 1e-10;
 const db pi = acos(0) * 2;
 
