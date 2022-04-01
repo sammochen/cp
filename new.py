d587872e88f41e5dfb43a6b4857846dd3e839190
Shih-Hao Chen <samuelshchen@gmail.com>
2022-04-01 21:12:43 +1100

    del in and out

--- new.py
+++ new.py
@@ -1,7 +1,6 @@
 import argparse
 import glob
 import os
-from time import time
 
 
 def get_args():
@@ -11,7 +10,7 @@ def get_args():
     return parser.parse_args()
 
 
-def reset(args):
+def make_main(args):
     os.system("cp body/top.cpp main.cpp")
 
     if args.template:
@@ -21,6 +20,19 @@ def reset(args):
         os.system("cat body/bottom-goog.cpp >> main.cpp")
     else:
         os.system("cat body/bottom.cpp >> main.cpp")
+
+
+def delete_in_out():
+    os.system("rm *.out 2> /dev/null")
+
+    input_files = glob.glob("*.in")
+    for input_file in input_files:
+        os.system(f"> {input_file}")
+
+
+def reset(args):
+    make_main(args)
+    delete_in_out()
 
 
 if __name__ == "__main__":
