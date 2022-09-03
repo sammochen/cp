66973b28ffb6f679f4fedd010077a5b89c80c9b2
Shih-Hao Chen <samuelshchen@gmail.com>
2022-09-03 12:32:34 +1000

    wip

--- templates/sieve.cpp
+++ templates/sieve.cpp
@@ -1,13 +1,16 @@
+
 namespace Sieve {
 const ll nax = 1005;
 ll inited = 0;
 ll prevPrime[nax * nax];
+ll phiMemo[nax * nax];
 
 void init() {
     if (inited) return;
     inited = 1;
 
     memset(prevPrime, -1, sizeof(prevPrime));
+    memset(phiMemo, -1, sizeof(phiMemo));
     for (ll x = 2; x < nax * nax; x++) {
         // not prime
         if (prevPrime[x] != -1) continue;
@@ -36,4 +39,22 @@ umap<ll, ll> decomp(const ll x) {
     }
     return ans;
 }
+
+// The number of y < x where y is relatively prime to x
+ll phi(ll x) {
+    init();
+
+    if (x <= 1) return 0;
+
+    ll& ans = phiMemo[x];
+    if (ans != -1) return ans;
+    ans = x;
+    auto M = decomp(x);
+    for (auto p : M) {
+        ans /= p.first;
+        ans *= p.first - 1;
+    }
+    return ans;
+}
+
 }  // namespace Sieve
