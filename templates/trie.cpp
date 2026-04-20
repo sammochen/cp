6739c03dc4252b40379abe04a45108898d6c0ecd
Shih-Hao Chen <samuelshchen@gmail.com>
2026-04-21 09:52:05 +1000

    wip

--- templates/trie.cpp
+++ templates/trie.cpp
@@ -1,20 +1,57 @@
 
+
+namespace Trie {
+
 struct tnode {
-    vector<unique_ptr<tnode>> children;
+    constexpr static NUM_CHILDREN = 26;
+    int children[NUM_CHILDREN];
+    int ind = -1;
+
     tnode() {
-        children.resize(26);
+        rep(i, 0, NUM_CHILDREN) {
+            children[i] = -1;
+        }
     }
 };
 
-using T = unique_ptr<tnode>;
+tnode nodes[1'000'005];
 
-void insert(tnode* root, const string& key) {
-    tnode* cur = root;
-    for (int i = 0; i < key.size(); i++) {
-        int index = key[i] - 'a';
-        if (!cur->children[index]) {
-            cur->children[index] = make_unique<tnode>();
-        }
-        cur = cur->children[index].get();
-    }
+ll node_index = 0;
+int make_tnode() {
+    nodes[node_index] = tnode();
+    return node_index++;
 }
+
+void reset_tnode() {
+    node_index = 0;
+}
+
+void insert(int root, const string& key) {
+    const ll n = key.size();
+    int cur = root;
+
+    rep(i, 0, n) {
+        int index = key[i] - 'a';
+        if (nodes[cur].children[index] == -1) {
+            nodes[cur].children[index] = make_tnode();
+        }
+        cur = nodes[cur].children[index];
+    }
+    nodes[cur].ind = ind;
+}
+
+void check(int root, ll dir, const string& key) {
+    const ll n = key.size();
+    int cur = root;
+    rep(i, 0, n) {
+        int index = key[i] - 'a';
+        if (nodes[cur].children[index] == -1) {
+            // no path
+            return;
+        }
+        cur = nodes[cur].children[index];
+    }
+    // yes path
+}
+
+}  // namespace Trie
